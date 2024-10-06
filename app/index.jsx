98823488234d66d67af73e2e74d716eaba7aa3b0
ZenXen7 <karlchristian.ajero@cit.edu>
2024-10-06 11:09:30 +0800

    added buttons

--- app/index.jsx
+++ app/index.jsx
@@ -1,40 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, Image, ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView
         contentContainerStyle={{
-          height: "100%",
+          flexGrow: 1, 
+          justifyContent: 'center', 
+          alignItems: 'center',
         }}
       >
-        <View className="w-full justify-start items-center h-full px-4">
+        <View className="w-full items-center px-4">
           <Image
             source={images.hblogo2}
-            className="mt-4 w-[250px] h-[90px]"
+            className="mb-4 w-[250px] h-[90px]"
             resizeMode="contain"
-          ></Image>
-
-          <Image 
-          source={images.hcard}
-          className="mt-6 max-w-[400px] w-full h-[320px]"
-          resizeMode="contain"
-          
           />
 
-            <View className="relative">
-              <Text className="text-3xl text-black font-sfbold text-center">Your all-in-one medical history tracker</Text>
-            </View>
+          <Image
+            source={images.hcard}
+            className="max-w-[400px] w-full h-[320px] mb-6"
+            resizeMode="contain"
+          />
+
+          <View className="relative">
+            <Text className="text-3xl text-black font-sfbold text-center">
+              Your all-in-one medical history tracker
+            </Text>
+          </View>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
         </View>
-
-
       </ScrollView>
+      <StatusBar backgroundColor="#000000" style="dark" />
     </SafeAreaView>
   );
 }
